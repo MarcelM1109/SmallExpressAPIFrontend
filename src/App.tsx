7138ee5ea93868f944b67956700df513dd64eea8
Marcel Mende <marcel.mende0@gmail.com>
2025-11-27 11:00:29 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import AuthLayout from './layouts/authLayout'
+import LoginPage from './pages/auth/login'
+import RegisterPage from './pages/auth/register'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/auth" element={<AuthLayout />}>
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
